4814ca4dcc533bdfb859748a87c16990070842bd
burch-cm <christopher.m.burch@gmail.com>
2016-05-30 14:49:27 -0400

    sampling data

---
+++ datasample.R
@@ -0,0 +1,36 @@
+wd <- "~/Documents/code/capstone"
+if(getwd() != wd) setwd(wd)
+
+# reads in a percentage of a target file
+sampleLines <- function(filename, percent = 0.1){
+    rows <- as.numeric(system(paste("wc -l < ", filename, sep=""), TRUE))
+    file.con <- file(filename)
+    sel <- round(percent * rows)
+    remaining <- rows
+    recs <- sort(sample(1:remaining, sel))
+    skip <- diff(c(1, recs)) - 1
+    mysel <- vector('character', sel)
+    for (i in 1:sel){
+        mysel[i] <- scan(file.con, what="", sep="\n", skip=skip[i], n=1, quiet=TRUE)
+    }
+    close(file.con)
+    return(mysel)
+}
+
+# specifiy the target files
+set.seed(100)
+file1 <- "./data/final/en_US/en_US.blogs.txt"
+file2 <- "./data/final/en_US/en_US.news.txt"
+file3 <- "./data/final/en_US/en_US.twitter.txt"
+
+blog.sample <- sampleLines(file1, percent = .1)
+write.csv(as.data.frame(blog.sample), "./data/blogsample.csv")
+rm(blog.sample)
+
+news.sample <- sampleLines(file2, percent = .1)
+write.csv(as.data.frame(news.sample), "./data/newssample.csv")
+rm(news.sample)
+
+twitter.sample <- sampleLines(file3, percent = .1)
+write.csv(as.data.frame(twitter.sample), "./data/twittersample.csv")
+rm(twitter.sample)
