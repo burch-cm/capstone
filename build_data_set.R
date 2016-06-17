33314dbf6cf279430d4661c7b8db69eb4e78764a
burch-cm <christopher.m.burch@gmail.com>
2016-06-17 10:31:05 -0400

    trigram backoff model

---
+++ build_data_set.R
@@ -0,0 +1,2 @@
+source("data_munging2.R")
+source("data_smushing2.R")
--- data_munging2.R
+++ data_munging2.R
@@ -12,7 +12,7 @@ files <- sapply(fn, f1)
 
 for(i in 1:length(files)){
     rows <- as.numeric(system(paste("wc -l < ", files[i], sep=""), TRUE))
-    chunksize <- 1000 # how many lines to read in at once
+    chunksize <- 2000 # how many lines to read in at once
     nchunks <- ceiling(rows/chunksize) # how many total iterations
     start <- 0
     pb1 <- txtProgressBar(min = 0, max = nchunks*length(files), style = 3)
@@ -22,28 +22,29 @@ for(i in 1:length(files)){
         mytext <- gsub("[^a-zA-z]", " ", mytext) # remove non-alpha
         mytext <- stripWhitespace(mytext)
         mytext <- gsub("^\\s+|\\s+$", "", mytext) # leading and trailing whitespace
-        n3 <- ngram_asweka(mytext, min = 1, max = 3)
+        n4 <- ngram_asweka(mytext, min = 4, max = 4)
         rm(mytext) # free up some space
-        n3 <- data.table(term = n3)
-        weights <- n3[, .N, by = term]
+        n4 <- data.table(term = n4)
+        weights <- n4[, .N, by = term]
         setnames(weights, old = names(weights), new = c("term", "weight"))
-        n3[, c('t1', 't2', 't3') := tstrsplit(term, " ")]
-        n3 <- unique(n3)
-        setkey(n3, term)
+        n4[, c('t1', 't2', 't3', 't4') := tstrsplit(term, " ")]
+        setkey(n4, term)
+        n4 <- unique(n4)
         setkey(weights, term)
         # merge unique terms and weights
-        terms <- merge(weights, n3, all.x = TRUE)
+        terms <- merge(weights, n4, all.x = TRUE)
         # termlist
-        termlist <- terms[, list(t1, t2, t3, weight)]
+        termlist <- terms[, list(t1, t2, t3, t4, weight)]
         setorderv(termlist, c("t1", "weight"), c(1, -1))
-        rm(terms, n3, weights)
+        rm(terms, n4, weights)
         # write termlist to file
         outname <- paste("./data/chunks/", fn[i], j, ".csv", sep ="")
         write.csv(termlist, outname)
         rm(termlist)
         start <- start + chunksize
-        setTxtProgressBar(pb1, value = 1)
+        setTxtProgressBar(pb1, value = i*j)
     }
+    print("Wrote chunks to ./data/chunks")
     close(pb1)
 }
 
