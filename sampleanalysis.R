2a4cebe941cc25e1f0cadcfe79045c5145cb44e7
Chris "Kitt" Burch <christopher.m.burch@gmail.com>
2016-06-20 10:09:02 -0400

    Delete sampleanalysis.R

--- sampleanalysis.R
+++
@@ -1,7 +0,0 @@
-library(data.table)
-library(magrittr)
-# load in sample data set
-samples <- fread("./data/sampledata.csv", col.names = c("line", "text", "source"))
-samples[, line := as.numeric(line)]
-samples[, c("source") := as.factor(source)]
-
